b38ed5dcb4540bd2f5087a62d97baa9af57f9229
Vinícius Albert <vinifantal@hotmail.com>
2022-10-05 22:44:13 -0300

    :newspaper: adding new menu selector to ticket message

--- src/main/java/project/kazumy/realhosting/discord/services/ticket/manager/TicketManager.java
+++ src/main/java/project/kazumy/realhosting/discord/services/ticket/manager/TicketManager.java
@@ -5,18 +5,19 @@ import lombok.SneakyThrows;
 import lombok.val;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
+import net.dv8tion.jda.api.interactions.components.selections.SelectMenu;
 import project.kazumy.realhosting.discord.InitBot;
 import project.kazumy.realhosting.discord.configuration.Configuration;
 import project.kazumy.realhosting.discord.services.BaseService;
 import project.kazumy.realhosting.discord.services.ticket.BaseTicket;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.awt.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Getter
+@SuppressWarnings("all")
 public class TicketManager extends BaseService {
 
     private final Set<BaseTicket> ticket = new HashSet<>();
@@ -30,6 +31,8 @@ public class TicketManager extends BaseService {
             consumer.addDefault("bot.guild.ticket.title", "Title");
             consumer.addDefault("bot.guild.ticket.footer", "Footer");
             consumer.addDefault("bot.guild.ticket.description", "Hello World \\n Zumo was here!");
+            consumer.addDefault("bot.guild.ticket.thumbnail", "thumbnail-url");
+            consumer.addDefault("bot.guild.ticket.site", "site-url");
             consumer.addDefault("bot.guild.ticket.fields.1.name", "My Name is");
             consumer.addDefault("bot.guild.ticket.fields.1.value", "Zumo");
             consumer.addDefault("bot.guild.ticket.fields.1.inline", true);
@@ -54,7 +57,7 @@ public class TicketManager extends BaseService {
             val embed = new EmbedBuilder();
             embed.setTitle(title);
             embed.setFooter(footer);
-            embed.setDescription(description);
+            embed.setColor(Color.YELLOW);
             val configSection = config.getConfigurationSection(section + "fields").getKeys(true);
 
             for (int i = 1; i <= configSection.size(); i++) {
@@ -65,8 +68,19 @@ public class TicketManager extends BaseService {
                 if (name == null || value == null || inline == null) continue;
                 embed.addField(name, value, Boolean.parseBoolean(String.valueOf(inline)));
             }
+            embed.setDescription(description);
+            embed.setThumbnail(config.getString(section + "thumbnail"));
+            embed.setAuthor("RealHosting", config.getString(section + "site"), config.getString(section + "thumbnail"));
+            embed.setFooter(config.getString(section + "footer"), config.getString(section + "thumbnail"));
 
-            textChannel.sendMessageEmbeds(embed.build()).queue();
+            val menu = SelectMenu.create("menu-ticket")
+                    .addOption("Adquirir Serviços", "real.buy", "Contrate um dos planos do nosso serviço de hospedagem.", Emoji.fromUnicode("U+1F4B5"))
+                    .addOption("Não... sério, preciso de ajuda!", "real.support", "Contate-nos pra solucionar problemas em seu serviço.", Emoji.fromUnicode("U+1F4BB"))
+                    .addOption("Esclarecer Dúvidas", "real.help", "Acabe com aquela pulga atrás da orelha.", Emoji.fromUnicode("U+1F4A1"))
+                    .addOption("Crítica Construtiva", "real.suggest", "Dê uma crítica construtiva em algo que podemos melhorar.", Emoji.fromUnicode("U+1F48C"))
+                    .build();
+
+            textChannel.sendMessageEmbeds(embed.build()).addActionRow(menu).queue();
         });
     }
 
