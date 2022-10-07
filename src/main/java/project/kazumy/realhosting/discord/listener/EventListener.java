96be5a7f89f1ba9af04369ee5887d66f0fde79cf
Vinícius Albert <vinifantal@hotmail.com>
2022-10-06 23:25:54 -0300

    :electric_plug: adding new Listener for interactions

--- src/main/java/project/kazumy/realhosting/discord/listener/EventListener.java
+++ src/main/java/project/kazumy/realhosting/discord/listener/EventListener.java
@@ -3,11 +3,24 @@ package project.kazumy.realhosting.discord.listener;
 import net.dv8tion.jda.api.events.interaction.component.SelectMenuInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+import project.kazumy.realhosting.discord.InitBot;
+import project.kazumy.realhosting.discord.listener.interactions.MenuInteraction;
+import project.kazumy.realhosting.discord.services.ticket.Ticket;
 
 public class EventListener extends ListenerAdapter {
 
+    private final InitBot instance;
+    public EventListener(InitBot instance) {
+        this.instance = instance;
+    }
+
     @Override
     public void onSelectMenuInteraction(@NotNull SelectMenuInteractionEvent event) {
-        super.onSelectMenuInteraction(event);
+        InitBot.interactionManager.getInteractionList()
+                .stream()
+                .filter(interaction -> interaction.getId().equals(event.getSelectMenu().getId()))
+                .findAny()
+                .ifPresentOrElse(interaction -> ((MenuInteraction)interaction).execute(event),
+                        () -> System.out.println("Unloaded interaction id: " + event.getSelectMenu().getId()));
     }
 }
