c45bff560032362fae96388a3c257b0e3ce26dc4
Vinícius Albert <vinifantal@hotmail.com>
2022-10-08 15:59:29 -0300

    :gear: merging the class Ticket and BaseTicket

--- src/main/java/project/kazumy/realhosting/discord/services/ticket/Ticket.java
+++ src/main/java/project/kazumy/realhosting/discord/services/ticket/Ticket.java
@@ -1,13 +1,38 @@
 package project.kazumy.realhosting.discord.services.ticket;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.SneakyThrows;
+import lombok.val;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
+import project.kazumy.realhosting.discord.configuration.Configuration;
 
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
 import java.util.List;
 
-public class Ticket extends BaseTicket {
+@Data
+@Builder()
+public class Ticket {
 
-    Ticket(String id, String category, Member author, boolean isClosed, List<Message> history) {
-        super(id, category, author, isClosed, history);
+    private String id, category, channelId;
+
+    private Member author;
+
+    private boolean isClosed;
+
+    private List<Message> history;
+
+    @SneakyThrows
+    public void saveTicket() {
+        val config = new Configuration("services/tickets/" + author.getId() + "/" + author.getEffectiveName() + "-" + id + ".yml")
+                .buildIfNotExists();
+        Collections.reverse(history);
+        history.forEach(message -> {
+            val date = message.getTimeCreated().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+            config.set("ID: " + message.getId() + " " + date + " - " + message.getAuthor().getAsTag() + ": ", message.getContentStripped());
+        });
+        config.save();
     }
 }
