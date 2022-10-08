d9ac8201f433fa4dcdc7fc07d3fc601b2c7fa536
Vinícius Albert <vinifantal@hotmail.com>
2022-10-08 16:00:47 -0300

    :gear: add new parameter with configuration instance

--- src/main/java/project/kazumy/realhosting/discord/services/BaseService.java
+++ src/main/java/project/kazumy/realhosting/discord/services/BaseService.java
@@ -2,11 +2,12 @@ package project.kazumy.realhosting.discord.services;
 
 import lombok.Data;
 import net.dv8tion.jda.api.JDA;
+import project.kazumy.realhosting.discord.configuration.Configuration;
 
 @Data
 public abstract class BaseService {
 
     private boolean operating;
 
-    public abstract BaseService loadService(JDA jda);
+    public abstract BaseService loadService(JDA jda, Configuration config);
 }
