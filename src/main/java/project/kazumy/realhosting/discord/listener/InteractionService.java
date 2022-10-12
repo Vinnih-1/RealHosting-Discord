8414142f719eec20ee6c90b9fdae36799e5d284e
Vinícius Albert <vinifantal@hotmail.com>
2022-10-12 02:07:56 -0300

    :gear: removed unused method

--- src/main/java/project/kazumy/realhosting/discord/listener/InteractionService.java
+++ src/main/java/project/kazumy/realhosting/discord/listener/InteractionService.java
@@ -2,8 +2,6 @@ package project.kazumy.realhosting.discord.listener;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
 @AllArgsConstructor
@@ -12,6 +10,4 @@ public abstract class InteractionService<T> {
     private final String id;
 
     public abstract void execute(T event);
-
-    public abstract void register(InteractionService<T> instance);
 }
