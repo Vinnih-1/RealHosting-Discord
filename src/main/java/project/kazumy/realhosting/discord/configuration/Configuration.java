1aaa5f3d7607a398af7d24438729ced462e04b42
Vinícius Albert <vinifantal@hotmail.com>
2022-10-11 23:01:26 -0300

    :gear: new method to request a ready embed message from yml file

--- src/main/java/project/kazumy/realhosting/discord/configuration/Configuration.java
+++ src/main/java/project/kazumy/realhosting/discord/configuration/Configuration.java
@@ -1,8 +1,11 @@
 package project.kazumy.realhosting.discord.configuration;
 
 import lombok.SneakyThrows;
+import lombok.val;
+import net.dv8tion.jda.api.EmbedBuilder;
 import org.simpleyaml.configuration.file.YamlFile;
 
+import java.awt.*;
 import java.util.function.Consumer;
 
 public class Configuration extends YamlFile {
@@ -23,5 +26,34 @@ public class Configuration extends YamlFile {
         consumer.accept(this);
         this.save();
         return this;
+    }
+
+    public EmbedBuilder getEmbedMessageFromConfig(Configuration config, String path) {
+        val section = "bot.guild." + path + ".";
+        val title = config.getString(section + "title");
+        val footer = config.getString(section + "footer");
+        val description = config.getString(section + "description");
+
+        val embed = new EmbedBuilder();
+
+        embed.setTitle(title);
+        embed.setFooter(footer);
+        embed.setColor(Color.YELLOW);
+        val configSection = config.getConfigurationSection(section + "fields").getKeys(true);
+
+        for (int i = 1; i <= configSection.size(); i++) {
+            val name = config.getString(section + "fields." + i + ".name");
+            val value = config.getString(section + "fields." + i + ".value");
+            val inline = config.get(section + "fields." + i + ".inline");
+
+            if (name == null || value == null || inline == null) continue;
+            embed.addField(name, value, Boolean.parseBoolean(String.valueOf(inline)));
+        }
+        embed.setDescription(description);
+        embed.setThumbnail(config.getString(section + "thumbnail"));
+        embed.setAuthor("RealHosting", config.getString(section + "site"), config.getString(section + "thumbnail"));
+        embed.setFooter(config.getString(section + "footer"), config.getString(section + "thumbnail"));
+
+        return embed;
     }
 }
