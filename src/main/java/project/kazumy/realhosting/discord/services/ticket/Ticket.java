c740d9add0a13e4e8a12ca72b1af3b1b0919c794
Vinícius Albert <vinifantal@hotmail.com>
2022-10-12 02:04:08 -0300

    :gear: folder to save closed tickets changed

--- src/main/java/project/kazumy/realhosting/discord/services/ticket/Ticket.java
+++ src/main/java/project/kazumy/realhosting/discord/services/ticket/Ticket.java
@@ -6,11 +6,16 @@ import lombok.SneakyThrows;
 import lombok.val;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageHistory;
+import project.kazumy.realhosting.discord.InitBot;
 import project.kazumy.realhosting.discord.configuration.Configuration;
 
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Data
 @Builder()
@@ -20,19 +25,29 @@ public class Ticket {
 
     private Member author;
 
-    private boolean isClosed;
-
     private List<Message> history;
 
     @SneakyThrows
     public void saveTicket() {
-        val config = new Configuration("services/tickets/" + author.getId() + "/" + author.getEffectiveName() + "-" + id + ".yml")
+        val channel = InitBot.ticketManager.getJda().getTextChannelById(this.getChannelId());
+        val chatHistory = MessageHistory.getHistoryFromBeginning(channel).submit();
+        this.setHistory(new ArrayList<>(chatHistory.get().getRetrievedHistory()));
+
+        InitBot.ticketManager.destroyRecordedTicket(this);
+        InitBot.ticketManager.getTicketMap().remove(this.getAuthor().getId());
+
+        val config = new Configuration("services/tickets/saved-ticket/" + author.getId() + "/" + author.getEffectiveName() + "-" + id + ".yml")
                 .buildIfNotExists();
+
         Collections.reverse(history);
+
         history.forEach(message -> {
-            val date = message.getTimeCreated().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+            val date = message.getTimeCreated()
+                    .format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")
+                    .withZone(ZoneId.of("America/Sao_Paulo")));
             config.set("ID: " + message.getId() + " " + date + " - " + message.getAuthor().getAsTag() + ": ", message.getContentStripped());
         });
         config.save();
+        channel.delete().queueAfter(5, TimeUnit.SECONDS);
     }
 }
