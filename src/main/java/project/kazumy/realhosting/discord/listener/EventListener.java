902ed3f91dda2abf4ab4032d68d6f7986f860d6a
Vinícius Albert <vinifantal@hotmail.com>
2022-10-12 02:06:19 -0300

    :heavy_check_mark: new method listener to button interaction

--- src/main/java/project/kazumy/realhosting/discord/listener/EventListener.java
+++ src/main/java/project/kazumy/realhosting/discord/listener/EventListener.java
@@ -1,11 +1,10 @@
 package project.kazumy.realhosting.discord.listener;
 
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.component.SelectMenuInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 import project.kazumy.realhosting.discord.InitBot;
-import project.kazumy.realhosting.discord.listener.interactions.MenuInteraction;
-import project.kazumy.realhosting.discord.services.ticket.Ticket;
 
 public class EventListener extends ListenerAdapter {
 
@@ -20,7 +19,17 @@ public class EventListener extends ListenerAdapter {
                 .stream()
                 .filter(interaction -> interaction.getId().equals(event.getSelectMenu().getId()))
                 .findAny()
-                .ifPresentOrElse(interaction -> ((MenuInteraction)interaction).execute(event),
+                .ifPresentOrElse(interaction -> ((InteractionService<SelectMenuInteractionEvent>)interaction).execute(event),
                         () -> System.out.println("Unloaded interaction id: " + event.getSelectMenu().getId()));
+    }
+
+    @Override
+    public void onButtonInteraction(@NotNull ButtonInteractionEvent event) {
+        InitBot.interactionManager.getInteractionList()
+                .stream()
+                .filter(interaction -> interaction.getId().equals(event.getComponentId()))
+                .findAny()
+                .ifPresentOrElse(interaction -> ((InteractionService<ButtonInteractionEvent>)interaction).execute(event),
+                        () -> System.out.println("Unloaded interaction id: " + event.getComponentId()));
     }
 }
