d3239658d012f78b1a5995dedd03d3c793f1f0c6
Vinícius Albert <vinifantal@hotmail.com>
2022-10-23 22:21:52 -0300

    :white_check_mark: new method to execute slash command and new list with all slash commands options

--- src/main/java/project/kazumy/realhosting/discord/commands/base/BaseSlashCommand.java
+++ src/main/java/project/kazumy/realhosting/discord/commands/base/BaseSlashCommand.java
@@ -1,15 +1,23 @@
 package project.kazumy.realhosting.discord.commands.base;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import project.kazumy.realhosting.discord.InitBot;
 
+import java.util.List;
+
 @Data
+@AllArgsConstructor
 public abstract class BaseSlashCommand {
 
     public final String name, description;
 
-    public final Permission permission;
+    private final List<OptionData> options;
+
+    public abstract void execute(SlashCommandInteractionEvent event);
 
     public void register() {
         InitBot.commandManager.addSlashCommand(this);
