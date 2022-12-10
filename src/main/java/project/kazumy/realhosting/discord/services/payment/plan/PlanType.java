a22506169d842071d6a2294497bc6d6e87f19073
Vinnih-1 <vinifantal@hotmail.com>
2022-12-10 15:12:42 -0300

    :bug: plan name were changed

--- src/main/java/project/kazumy/realhosting/discord/services/payment/plan/PlanType.java
+++ src/main/java/project/kazumy/realhosting/discord/services/payment/plan/PlanType.java
@@ -12,8 +12,8 @@ public enum PlanType {
     BASIC(100L, 1L, 5L, 1L, 1L, DataType.GB),
     MEDIAN(100L, 2L, 5L, 1L, 1L, DataType.GB),
     ADVANCED(200L, 4L, 10L, 5L, 2L, DataType.GB),
-    ULTRA(400L, 8L, 10L, 5L, 2L, DataType.GB),
-    MEGA(600L, 16L, 10L, 10L, 4L, DataType.GB);
+    MEGA(400L, 8L, 10L, 5L, 2L, DataType.GB),
+    ULTRA(600L, 16L, 10L, 10L, 4L, DataType.GB);
 
     @Getter private long cpu, memory, disk, backup, database;
     @Getter private DataType dataType;
