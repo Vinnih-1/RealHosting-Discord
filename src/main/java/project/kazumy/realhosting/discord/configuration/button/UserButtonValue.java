edc149af6d41873686f99cfa9ea91897c948fe89
Vinnih-1 <vinifantal@hotmail.com>
2023-01-09 19:51:20 -0300

    :white_check_mark: starting new configuration to get buttons from yaml file

---
+++ src/main/java/project/kazumy/realhosting/discord/configuration/button/UserButtonValue.java
@@ -0,0 +1,24 @@
+package project.kazumy.realhosting.discord.configuration.button;
+
+import com.henryfabio.minecraft.configinjector.common.annotations.ConfigFile;
+import com.henryfabio.minecraft.configinjector.common.annotations.ConfigSection;
+import com.henryfabio.minecraft.configinjector.common.injector.ConfigurationInjectable;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+import java.util.function.Function;
+
+@Getter
+@Accessors(fluent = true)
+@ConfigSection("menu")
+@ConfigFile("menu.yml")
+public class UserButtonValue implements ConfigurationInjectable {
+
+    public static final UserButtonValue instance = new UserButtonValue();
+
+    
+
+    public static <T> T get(Function<UserButtonValue, T> function) {
+        return function.apply(instance);
+    }
+}
