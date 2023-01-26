45e7b04c2b81adfd02c58d9bf65e5c77452e4d06
Vinnih-1 <vinifantal@hotmail.com>
2023-01-26 01:25:23 -0300

    :wrench: method renamed to `updateTicket`

--- src/main/java/project/kazumy/realhosting/discord/listener/interactions/menu/FeedbackMenuInteraction.java
+++ src/main/java/project/kazumy/realhosting/discord/listener/interactions/menu/FeedbackMenuInteraction.java
@@ -1,7 +1,6 @@
 package project.kazumy.realhosting.discord.listener.interactions.menu;
 
 import lombok.val;
-import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent;
 import net.dv8tion.jda.api.interactions.components.text.TextInput;
 import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
@@ -24,7 +23,7 @@ public class FeedbackMenuInteraction extends InteractionService<StringSelectInte
         val selectedOption = event.getSelectedOptions().get(0);
         val ticket = discordMain.getTicketManager().findTicketByChatId(channelId);
         ticket.setFeedback(selectedOption.getEmoji().getFormatted() + " " + selectedOption.getLabel());
-        discordMain.getTicketManager().saveFeedback(ticket);
+        discordMain.getTicketManager().updateTicket(ticket);
 
         val modal = Modal.create("feedback-ticket-modal", "Avaliação de Atendimento")
                 .addActionRow(
