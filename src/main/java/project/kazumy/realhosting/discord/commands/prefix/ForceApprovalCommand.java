caaa4ba73361f0f78aa51494bee781d34e637a9c
Vinnih-1 <vinifantal@hotmail.com>
2023-01-27 02:09:12 -0300

    :white_check_mark: new prefix command to approval sales

---
+++ src/main/java/project/kazumy/realhosting/discord/commands/prefix/ForceApprovalCommand.java
@@ -0,0 +1,39 @@
+package project.kazumy.realhosting.discord.commands.prefix;
+
+import lombok.val;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+import project.kazumy.realhosting.discord.DiscordMain;
+import project.kazumy.realhosting.discord.commands.base.BasePrefixCommand;
+import project.kazumy.realhosting.model.payment.intent.PaymentIntent;
+
+import java.awt.*;
+
+public class ForceApprovalCommand extends BasePrefixCommand {
+
+    private final DiscordMain discordMain;
+
+    public ForceApprovalCommand(DiscordMain discordMain) {
+        super("forceapproval", Permission.ADMINISTRATOR, discordMain);
+        this.discordMain = discordMain;
+    }
+
+    @Override
+    public void execute(Member member, Message message, String[] args) {
+        if (args.length < 2) return;
+        val client = discordMain.getClientManager().getClientById(args[0]);
+        discordMain.getPlanService().getPlanByClientId(args[0]).stream()
+                .filter(plan -> plan.getId().equals(args[1])).findFirst()
+                .ifPresentOrElse(plan -> {
+                    plan.setPaymentIntent(PaymentIntent.FORCE_APPROVAL);
+                    plan.savePlan(client);
+                }, () -> {
+                    message.getChannel().sendMessageEmbeds(new EmbedBuilder()
+                                    .setColor(Color.RED)
+                                    .setDescription("<@%s>, não consegui encontrar nenhum plano com este identificador!")
+                            .build()).queue();
+                });
+    }
+}
