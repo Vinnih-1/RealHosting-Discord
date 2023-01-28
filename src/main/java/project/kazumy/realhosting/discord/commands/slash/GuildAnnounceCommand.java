4adfc5d508d328e2223d9713031de7bb8770c690
Vinnih-1 <vinifantal@hotmail.com>
2023-01-28 02:24:43 -0300

    :white_check_mark: new announce command

--- src/main/java/project/kazumy/realhosting/discord/commands/slash/GuildAnnounceCommand.java
+++ src/main/java/project/kazumy/realhosting/discord/commands/slash/GuildAnnounceCommand.java
@@ -1,2 +1,26 @@
-package project.kazumy.realhosting.discord.commands.slash;public class GuildAnnounceCommand {
+package project.kazumy.realhosting.discord.commands.slash;
+
+import lombok.val;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.components.text.TextInput;
+import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
+import net.dv8tion.jda.api.interactions.modals.Modal;
+import project.kazumy.realhosting.discord.commands.base.BaseSlashCommand;
+
+public class GuildAnnounceCommand extends BaseSlashCommand {
+
+    public GuildAnnounceCommand() {
+        super("anunciar",
+                "Utilize este comando para fazer um anúncio",
+                null);
+    }
+
+    @Override
+    public void execute(SlashCommandInteractionEvent event) {
+        val modal = Modal.create("announce-command-modal", "Criar um Anúncio")
+                .addActionRow(TextInput.create("title", "Título", TextInputStyle.SHORT).build())
+                .addActionRow(TextInput.create("description", "Descrição", TextInputStyle.PARAGRAPH).build())
+                .build();
+        event.replyModal(modal).queue();
+    }
 }
