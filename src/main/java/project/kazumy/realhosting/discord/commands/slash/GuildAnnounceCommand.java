1f99ddaa1e31c623e6d3df4f723568466246c91f
Vinnih-1 <vinifantal@hotmail.com>
2023-01-28 14:55:24 -0300

    :white_check_mark: new command to announce messages

--- src/main/java/project/kazumy/realhosting/discord/commands/slash/GuildAnnounceCommand.java
+++ src/main/java/project/kazumy/realhosting/discord/commands/slash/GuildAnnounceCommand.java
@@ -1,22 +1,27 @@
 package project.kazumy.realhosting.discord.commands.slash;
 
 import lombok.val;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.components.text.TextInput;
 import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
 import net.dv8tion.jda.api.interactions.modals.Modal;
+import project.kazumy.realhosting.discord.DiscordMain;
 import project.kazumy.realhosting.discord.commands.base.BaseSlashCommand;
 
 public class GuildAnnounceCommand extends BaseSlashCommand {
 
-    public GuildAnnounceCommand() {
+    public GuildAnnounceCommand(DiscordMain discordMain) {
         super("anunciar",
                 "Utilize este comando para fazer um anúncio",
-                null);
+                null,
+                discordMain);
     }
 
     @Override
     public void execute(SlashCommandInteractionEvent event) {
+        if (!event.getMember().hasPermission(Permission.ADMINISTRATOR)) return;
+
         val modal = Modal.create("announce-command-modal", "Criar um Anúncio")
                 .addActionRow(TextInput.create("title", "Título", TextInputStyle.SHORT).build())
                 .addActionRow(TextInput.create("description", "Descrição", TextInputStyle.PARAGRAPH).build())
