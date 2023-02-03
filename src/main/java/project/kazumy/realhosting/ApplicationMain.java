3aae7b18668ed01664ceaf0df2e2d579e9dddb79
Vinnih-1 <vinifantal@hotmail.com>
2023-02-03 18:12:44 -0300

    :white_check_mark: update new main class

--- src/main/java/project/kazumy/realhosting/ApplicationMain.java
+++ src/main/java/project/kazumy/realhosting/ApplicationMain.java
@@ -1,18 +1,39 @@
 package project.kazumy.realhosting;
 
 import com.henryfabio.sqlprovider.executor.SQLExecutor;
-import lombok.val;
+import project.kazumy.realhosting.configuration.registry.ConfigurationRegistry;
 import project.kazumy.realhosting.database.SQLProvider;
 import project.kazumy.realhosting.discord.DiscordMain;
+import project.kazumy.realhosting.model.entity.client.manager.ClientManager;
+import project.kazumy.realhosting.model.entity.client.repository.ClientRepository;
+import project.kazumy.realhosting.model.panel.Panel;
+import project.kazumy.realhosting.model.payment.Payment;
+import project.kazumy.realhosting.model.plan.manager.PlanManager;
+import project.kazumy.realhosting.model.plan.repository.PlanRepository;
 
 public class ApplicationMain {
 
     private static SQLExecutor executor;
+    private static ClientManager clientManager;
+    private static PlanManager planManager;
+    private static Panel panel;
+    private static Payment payment;
 
     public static void main(String[] args) {
-        val application = new ApplicationMain();
+        new ConfigurationRegistry().register();
+        ApplicationMain.executor = SQLProvider.of().createDefaults();
+        ApplicationMain.planManager = new PlanManager(PlanRepository.of(executor)).loadPrePlan();
+        ApplicationMain.clientManager = ClientManager.of(ClientRepository.of(executor), planManager);
+        ApplicationMain.payment = Payment.of(clientManager, planManager);
+        planManager.setPayment(payment);
 
-        executor = SQLProvider.of(application).createDefaults();
-        new DiscordMain("MTA0NjQwOTU2OTczMzIwMTkzMQ.G-bg17.99VpctJRT7718XJMjvMSWGuBzmi2L31FjARKzk");
+        ApplicationMain.panel = new Panel();
+
+        DiscordMain.of(
+                executor,
+                clientManager,
+                planManager,
+                panel
+        ).startup("");
     }
 }
